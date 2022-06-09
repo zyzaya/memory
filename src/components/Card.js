6be1e27cabba98e28cfae2cd9791372989beffe4
Riley Sherlock <riley.sherlock@mts.net>
2022-06-09 14:41:53 -0500

    modify cards to use buttons

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,9 @@ import '../styles/Card.css';
 
 export default function Card(props) {
   return (
-    <div className="Card">
+    <button className="Card">
       <div className="Decor" style={{ backgroundColor: props.color }}></div>
       <div className="Info">{props.text}</div>
-    </div>
+    </button>
   );
 }
