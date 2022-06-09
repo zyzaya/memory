919cafef3b8d3be8e1a13b9500ed690182d25726
Riley Sherlock <riley.sherlock@mts.net>
2022-06-09 14:26:11 -0500

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import './styles/App.css';
 import Game from './components/Game';
+import Score from './components/Score';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <div className="GameInfo">
+          <h1>Colour Memory Game</h1>
+          <span>Click the colour but don't click the same one twice!</span>
+        </div>
+        <Score />
+      </header>
+
       <Game />
     </div>
   );
---
+++ src/components/Score.js
@@ -0,0 +1,14 @@
+export default function Score() {
+  return (
+    <div>
+      <div>
+        <span>Score:</span>
+        <span>0</span>
+      </div>
+      <div>
+        <span>Best Score:</span>
+        <span>0</span>
+      </div>
+    </div>
+  );
+}
