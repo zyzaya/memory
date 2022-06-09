6f247dba004e56edf4731d4e248626a54c0476a2
Riley Sherlock <riley.sherlock@mts.net>
2022-06-09 13:57:33 -0500

    create card styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,8 @@
+import '../styles/Card.css';
+
 export default function Card(props) {
   return (
-    <div className="card">
+    <div className="Card">
       <div className="Decor" style={{ backgroundColor: props.color }}></div>
       <span className="Info">{props.text}</span>
     </div>
