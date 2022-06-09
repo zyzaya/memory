1454d71824042098276c823e7f4070f27ce498c4
Riley Sherlock <riley.sherlock@mts.net>
2022-06-09 14:06:53 -0500

    add faker, please modules to generate cards

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,18 @@
 import Card from './Card';
+import Please from 'pleasejs';
+import { faker } from '@faker-js/faker';
 import '../styles/Game.css';
 
 export default function Game() {
-  let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+  let colors = [];
+  for (let i = 0; i < 12; i++) colors.push(Please.make_color());
+
+  console.log(faker.color);
 
   return (
     <div className="Game">
       {colors.map((color) => (
-        <Card color={color} text={color} />
+        <Card color={color} text={faker.name.findName()} />
       ))}
     </div>
   );
