2bbd9a8caeb78b09f90eaf71d6853a9fadbd3cae
Riley Sherlock <riley.sherlock@mts.net>
2022-06-09 14:52:42 -0500

    add key property to card in Game

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import '../styles/Card.css';
 
 export default function Card(props) {
   return (
-    <button className="Card">
+    <button className="Card" onClick={props.onclick}>
       <div className="Decor" style={{ backgroundColor: props.color }}></div>
       <div className="Info">{props.text}</div>
     </button>
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,35 @@
+import React, { useState } from 'react';
 import Card from './Card';
 import Please from 'pleasejs';
 import { faker } from '@faker-js/faker';
+import uniqid from 'uniqid';
 import '../styles/Game.css';
 
-export default function Game() {
-  let colors = [];
-  for (let i = 0; i < 12; i++) colors.push(Please.make_color());
+export default function Game(props) {
+  for (let i = 0; i < 12; i++) {
+    props.cards.push({
+      color: Please.make_color(),
+      text: faker.name.firstName(),
+      key: uniqid(),
+    });
+  }
+
+  const [clicked, setClicked] = useState([]);
+
+  function handleClick(key) {
+    if (!clicked.includes(key)) setClicked(clicked.concat(key));
+    console.log(clicked);
+  }
 
   return (
     <div className="Game">
-      {colors.map((color) => (
-        <Card color={color} text={faker.name.firstName()} />
+      {props.cards.map((c) => (
+        <Card
+          color={c.color}
+          text={c.text}
+          onclick={() => handleClick(c.key)}
+          key={c.key}
+        />
       ))}
     </div>
   );
